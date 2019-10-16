3868af7f1d0ef7c853382f5621e725cb081cbc9e
Nezeraudow <55483521+Nezeraudow@users.noreply.github.com>
2019-10-16 14:54:59 +0200

    Add files via upload

---
+++ SimulationFin/src/main/java/eu/eisti/fintech/one/SimulationFin/SimulationFinApplication.java
@@ -0,0 +1,13 @@
+package eu.eisti.fintech.one.SimulationFin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulationFinApplication.class, args);
+	}
+
+}
